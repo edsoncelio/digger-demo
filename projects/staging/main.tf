77741ea35628263d38dc36bc15a0b8b7bf205841
Edson C <edsoncelio@users.noreply.github.com>
2023-06-08 20:25:20 -0300

    feat: change acl to private (#1)
    auto-merge

--- projects/staging/main.tf
+++ projects/staging/main.tf
@@ -4,5 +4,11 @@ resource "aws_s3_bucket" "bucket" {
   tags = {
     Name        = "digger-demo-staging-bucket"
     Environment = "staging"
+    CreatedBy = "terraform"
   }
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  bucket = aws_s3_bucket.bucket.id
+  acl    = "private"
 }
